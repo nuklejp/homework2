2ff58785e90b9e16c71923ad75cb9126778a39cc
Daisuke Fujihara <nuklejp@yahoo.co.jp>
2019-07-09 06:49:50 +0000

    joij

---
+++ PHP12/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+});
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes(); 
+
+Route::get('/home', 'HomeController@index')->name('home');
